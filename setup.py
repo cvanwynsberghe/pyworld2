666e84b2fd7239cb58414aaa2b65ec28b0ba21d2
Charles Vanwynsberghe <charles@po_CharlesVWB.localdomain>
2020-10-26 13:08:34 +0100

    update for pypi install

--- setup.py
+++ setup.py
@@ -1,54 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import pyworld2
 
 setup(
-    name = 'pyworld2',
-    version = pyworld2.__version__,
+    name='pyworld2',
+    version=pyworld2.__version__,
+    packages=["pyworld2"],
+    description="A Python implementation of the model World2",
+    long_description=open('README.md').read(),
 
-
-    # Liste les packages à insérer dans la distribution
-    # plutôt que de le faire à la main, on utilise la foncton
-    # find_packages() de setuptools qui va cherche tous les packages
-    # python recursivement dans le dossier courant.
-    # C'est pour cette raison que l'on a tout mis dans un seul dossier:
-    # on peut ainsi utiliser cette fonction facilement
-    # packages = find_packages(),
-    packages = ["pyworld2"],
-
-    author = "Charles Vanwynsberghe",
-
-    # short description
-    description = "A Python implementation of the model World2",
-    # long description in README
-    long_description = open('README.md').read(),
-
-    # Vous pouvez rajouter une liste de dépendances pour votre lib
-    # et même préciser une version. A l'installation, Python essayera de
-    # les télécharger et les installer.
-    #
-    # Ex: ["numpy", "scipy", "matplotlib"]
-    #
-    # Dans notre cas on en a pas besoin, donc je le commente, mais je le
-    # laisse pour que vous sachiez que ça existe car c'est très utile.
-    install_requires = ["numpy", "scipy", "matplotlib"],
-
-    # Active la prise en compte du fichier MANIFEST.in
-    include_package_data = True,
-
-    # Une url qui pointe vers la page officielle de votre lib
+    author="Charles Vanwynsberghe",
     url='http://github.com/cvanwynsberghe/pyworld2',
+    download_url="https://github.com/cvanwynsberghe/pyworld2/archive/v1.0.tar.gz",
 
-    # Il est d'usage de mettre quelques metadata à propos de sa lib
-    # Pour que les robots puissent facilement la classer.
-    # La liste des marqueurs autorisées est longue:
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
-    #
-    # Il n'y a pas vraiment de règle pour le contenu. Chacun fait un peu
-    # comme il le sent. Il y en a qui ne mettent rien.
+    install_requires=["numpy", "scipy", "matplotlib"],
+
+    include_package_data=True,  # files declared in MANIFEST.in
+
     classifiers=[
         "Programming Language :: Python",
         "Natural Language :: English",
@@ -57,22 +28,5 @@ setup(
         "Topic :: Education",
     ],
 
-    # C'est un système de plugin, mais on s'en sert presque exclusivement
-    # Pour créer des commandes, comme "django-admin".
-    # Par exemple, si on veut créer la fabuleuse commande "proclame-sm", on
-    # va faire pointer ce nom vers la fonction proclamer(). La commande sera
-    # créé automatiquement.
-    # La syntaxe est "nom-de-commande-a-creer = package.module:fonction".
-    # entry_points = {
-    #     'console_scripts': [
-    #         'proclame-sm = sm_lib.core:proclamer',
-    #     ],
-    # },
-
-    # A fournir uniquement si votre licence n'est pas listée dans "classifiers"
-    # ce qui est notre cas
-    license = "GPL-3.0",
-
-    # Il y a encore une chiée de paramètres possibles, mais avec ça vous
-    # couvrez 90% des besoins
-)
+    license="MIT",
+    )
